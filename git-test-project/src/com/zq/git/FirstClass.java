6789b292fa0f356a5eb8b90218cd7f1c567b870a
zhangqi <zhangqi@powerpms.com>
2020-12-21 16:43:12 +0800

    修改firstClass

--- git-test-project/src/com/zq/git/FirstClass.java
+++ git-test-project/src/com/zq/git/FirstClass.java
@@ -2,11 +2,13 @@ package com.zq.git;
 
 public class FirstClass {
     public static void main(String[] args) {
+        //hello
         System.out.println("hello git ");
         System.out.println("hello git project");
     }
 
     public void test(){
         int i = 0;
+        System.out.println(i);
     }
 }
