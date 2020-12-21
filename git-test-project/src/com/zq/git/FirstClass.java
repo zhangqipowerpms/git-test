a45f01515b12a602cb779cdaa831a7b52264d525
zhangqi <zhangqi@powerpms.com>
2020-12-21 16:20:46 +0800

    提交git

---
+++ git-test-project/src/com/zq/git/FirstClass.java
@@ -0,0 +1,8 @@
+package com.zq.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+        System.out.println("hello git project");
+    }
+}
